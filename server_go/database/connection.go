826980d00676b159295331261ebfe51d27dca939
Juan Daniel Cardona <155476028+JuDanielCardona@users.noreply.github.com>
2024-03-25 19:48:38 -0500

    Update connection.go

--- server_go/database/connection.go
+++ server_go/database/connection.go
@@ -10,17 +10,12 @@ import (
 var DB *gorm.DB
 
 func Connection() {
-	host := "localhost"
-	// host := os.Getenv("DATABASE_URL")
-	// user := os.Getenv("USER")
-	// password := os.Getenv("PASSWORD")
-	// dbname := os.Getenv("DBNAME")
-	// port := os.Getenv("PORT")
+	host := os.Getenv("DATABASE_URL")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	dbname := os.Getenv("DBNAME")
+	port := os.Getenv("PORT")
 
-	user := "postgres"
-	password := "12345"
-	dbname := "users_db"
-	port := "5432"
 	var DSN = "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port
 	var error error
 	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
